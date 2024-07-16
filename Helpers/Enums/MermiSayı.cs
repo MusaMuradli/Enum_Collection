69eb6ef83e2705fe571a2a62a192b526d116f0f8
MusaCodeBB <musachm@code.edu.az>
2024-07-16 17:14:18 +0400

    first commit

---
+++ Helpers/Enums/MermiSayı.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_Collections.Helpers.Enums
+{
+    public enum MermiSayı
+    {
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,16 @@
+﻿using Enums_Collections.Helpers.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_Collections
+{
+    public class Weapon
+    {
+        public MermiNovu MermiNovleri { get; set; }
+        public MermiSayı MermiSayi { get; set; }
+    }
+    public 
+}
